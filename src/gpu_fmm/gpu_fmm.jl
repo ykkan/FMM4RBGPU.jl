3b88e505b4fd9a67d35830aad37548dab81a1231
Yi-Kai Kan <tw.ykkan@gmail.com>
2022-10-04 13:08:20 +0200

    Fix interaction.jl

--- src/gpu_fmm/gpu_fmm.jl
+++ src/gpu_fmm/gpu_fmm.jl
@@ -1,3 +1,4 @@
+include("gpu_interaction_lists.jl")
 include("gpu_upwardpass.jl")
 include("gpu_interaction.jl")
 include("gpu_downwardpass.jl")
--- src/gpu_fmm/gpu_interaction_lists.jl
+++ src/gpu_fmm/gpu_interaction_lists.jl
@@ -7,16 +7,17 @@ struct InteractionListsGPU{I}
     nm2lgroup::I
     p2p_lists::Vector{Tuple{I,I}}
     m2l_lists::Vector{Tuple{I,I}}
-    p2p_lists_ptrs::Vecotr{I}
-    m2l_lists_ptrs::Vecotr{I}
+    p2p_lists_ptrs::Vector{I}
+    m2l_lists_ptrs::Vector{I}
 end
 
 function ilistptrs(lists::Vector{Tuple{I,I}}, nelem::I) where {I}
     lists_ptrs = [1]
-    val = lists[1]
+    val = lists[1][1]
     for i in 1:nelem
-        if lists[i] != val
-            push![lists_ptrs, i]
+        if lists[i][1] != val
+            val = lists[i][1]
+            push!(lists_ptrs, i)
         end
     end
     push!(lists_ptrs, nelem+1)
@@ -35,5 +36,5 @@ function InteractionListsGPU(clusters::Clusters{I,T}; stretch=stretch, eta=eta) 
     m2l_lists_ptrs = ilistptrs(m2l_lists, nm2l)
     np2pgroup = length(p2p_lists_ptrs) - 1
     nm2lgroup = length(m2l_lists_ptrs) - 1
-    return InteractionListsGPU(np2p, nm2l, p2p_lists, m2l_lists, p2p_lists_ptrs, m2l_lists_ptrs, np2pgroup, nm2lgroup)
+    return InteractionListsGPU(np2p, nm2l, np2pgroup, nm2lgroup, p2p_lists, m2l_lists, p2p_lists_ptrs, m2l_lists_ptrs)
 end
