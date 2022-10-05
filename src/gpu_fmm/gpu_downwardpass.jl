e251588b570e3ec72861bb590ecf50018d2af5c7
Yi-Kai Kan <tw.ykkan@gmail.com>
2022-10-05 18:12:54 +0200

    Fix GC problem for downwardpass.jl

--- src/gpu_fmm/gpu_downwardpass.jl
+++ src/gpu_fmm/gpu_downwardpass.jl
@@ -1,5 +1,6 @@
 export gpu_L2L!, gpu_L2P!
 
+
 function gpu_L2L!(mp_efields::CuDeviceArray{SVector{3,T},4,1}, mp_bfields::CuDeviceArray{SVector{3,T},4,1}, degree::Val{D},
     cl_bboxes::CuDeviceVector{Tuple{SVector{3,T},SVector{3,T}},1},cl_parents::CuDeviceVector{I,1}, nodeindices::UnitRange{I}) where {I,T,D}
     bid = blockIdx().x
@@ -14,25 +15,12 @@ function gpu_L2L!(mp_efields::CuDeviceArray{SVector{3,T},4,1}, mp_bfields::CuDev
     c_y = c_bmin[2] + (cos(pi*(c_j-1)/D) + 1.0)/2.0 * (c_bmax[2] - c_bmin[2])
     c_z = c_bmin[3] + (cos(pi*(c_k-1)/D) + 1.0)/2.0 * (c_bmax[3] - c_bmin[3])
 
-    ws = @MVector zeros(T,D+1)
-    for i in 1:(D+1)
-        ws[i] = (i%2)==1 ? 1.0 : -1.0
-    end
-    ws[1] *= 0.5
-    ws[D+1] *= 0.5
+    ws = baryweights(Val(D+1), T)
 
-    p_xcoords = @MVector zeros(T,D+1)
-    p_ycoords = @MVector zeros(T,D+1)
-    p_zcoords = @MVector zeros(T,D+1)
-    for i in 1:(D+1)
-        p_xcoords[i] = p_bmin[1] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (p_bmax[1] - p_bmin[1])
-        p_ycoords[i] = p_bmin[2] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (p_bmax[2] - p_bmin[2])
-        p_zcoords[i] = p_bmin[3] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (p_bmax[3] - p_bmin[3])
-    end
+    p_xcoords = nsvector(i->cheb2coord(i, p_bmin[1], p_bmax[1], Val(D)), Val(D+1), T)
+    p_ycoords = nsvector(i->cheb2coord(i, p_bmin[2], p_bmax[2], Val(D)), Val(D+1), T)
+    p_zcoords = nsvector(i->cheb2coord(i, p_bmin[3], p_bmax[3], Val(D)), Val(D+1), T)
 
-    weight_x = @MVector zeros(T,D+1)
-    weight_y = @MVector zeros(T,D+1)
-    weight_z = @MVector zeros(T,D+1)
     sum_x = zero(T)
     sum_y = zero(T)
     sum_z = zero(T)
@@ -46,9 +34,6 @@ function gpu_L2L!(mp_efields::CuDeviceArray{SVector{3,T},4,1}, mp_bfields::CuDev
         wdx = ws[i] / dx
         wdy = ws[i] / dy
         wdz = ws[i] / dz
-        weight_x[i] = wdx
-        weight_y[i] = wdy
-        weight_z[i] = wdz
         xflag = abs(dx) < eps(T) ? i : xflag
         yflag = abs(dy) < eps(T) ? i : yflag
         zflag = abs(dz) < eps(T) ? i : zflag
@@ -57,33 +42,27 @@ function gpu_L2L!(mp_efields::CuDeviceArray{SVector{3,T},4,1}, mp_bfields::CuDev
         sum_z += wdz
     end
 
-    if xflag == -1
-        weight_x ./= sum_x
-    else
-        weight_x .= 0.0
-        weight_x[xflag] = 1.0
-    end
-
-    if yflag == -1
-        weight_y ./= sum_y
-    else
-        weight_y .= 0.0
-        weight_y[yflag] = 1.0
-    end
-
-    if zflag == -1
-        weight_z ./= sum_z
-    else
-        weight_z .= 0.0
-        weight_z[zflag] = 1.0
-    end
-
     c_e_sum = SVector{3,T}(0.0,0.0,0.0)
     c_b_sum = SVector{3,T}(0.0,0.0,0.0)
     for p_k in 1:(D+1)
+        if zflag == -1
+            wz = ws[p_k] /(c_z - p_zcoords[p_k]) / sum_z
+        else
+            wz = (p_k == zflag) ? 1.0 : 0.0
+        end
         for p_j in 1:(D+1)
+            if yflag == -1
+                wy = ws[p_j] /(c_y - p_ycoords[p_j]) / sum_y
+            else
+                wy = (p_j == yflag) ? 1.0 : 0.0
+            end
             for p_i in 1:(D+1)
-                weight = weight_x[p_i] * weight_y[p_j] * weight_z[p_k]
+                if xflag == -1
+                    wx = ws[p_i] /(c_x - p_xcoords[p_i]) / sum_x
+                else
+                    wx = (p_i == xflag) ? 1.0 : 0.0
+                end
+                weight = wx * wy * wz
                 c_e_sum += mp_efields[p_i,p_j,p_k,pindex] * weight
                 c_b_sum += mp_bfields[p_i,p_j,p_k,pindex] * weight
             end
@@ -99,36 +78,19 @@ function gpu_L2P!(pr_positions::CuDeviceVector{SVector{3,T},1}, pr_efields::CuDe
     cl_bboxes::CuDeviceVector{Tuple{SVector{3,T},SVector{3,T}},1}, cl_parlohis::CuDeviceVector{Tuple{I,I},1}, lfindices::UnitRange{I}) where {I,T,D}
     bid = blockIdx().x
     tid = threadIdx().x
-
     nodeindex = lfindices[bid]
-
     parlo, parhi = cl_parlohis[nodeindex]
-
-    mp_bmin, mp_bmax = cl_bboxes[nodeindex]
-    mp_xcoords = @MVector zeros(T,D+1)
-    mp_ycoords = @MVector zeros(T,D+1)
-    mp_zcoords = @MVector zeros(T,D+1)
-    for i in 1:(D+1)
-        mp_xcoords[i] = mp_bmin[1] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (mp_bmax[1] - mp_bmin[1])
-        mp_ycoords[i] = mp_bmin[2] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (mp_bmax[2] - mp_bmin[2])
-        mp_zcoords[i] = mp_bmin[3] + (cos(pi*(i-1)/D) + 1.0)/2.0 * (mp_bmax[3] - mp_bmin[3])
-    end
-
     npar = parhi - parlo + 1
     if tid <= npar
-        ws = @MVector zeros(T,D+1)
-        for i in 1:(D+1)
-            ws[i] = (i%2)==1 ? 1.0 : -1.0
-        end
-        ws[1] *= 0.5
-        ws[D+1] *= 0.5
+        mp_bmin, mp_bmax = cl_bboxes[nodeindex]
+        mp_xcoords = nsvector(i->cheb2coord(i, mp_bmin[1], mp_bmax[1], Val(D)), Val(D+1), T)
+        mp_ycoords = nsvector(i->cheb2coord(i, mp_bmin[2], mp_bmax[2], Val(D)), Val(D+1), T)
+        mp_zcoords = nsvector(i->cheb2coord(i, mp_bmin[3], mp_bmax[3], Val(D)), Val(D+1), T)
 
         i = ct_parindices[parlo + tid - 1]
         x, y, z = pr_positions[i]
 
-        weight_x = @MVector zeros(T,D+1)
-        weight_y = @MVector zeros(T,D+1)
-        weight_z = @MVector zeros(T,D+1)
+        ws = baryweights(Val(D+1), T)
         sum_x = zero(T)
         sum_y = zero(T)
         sum_z = zero(T)
@@ -142,9 +104,6 @@ function gpu_L2P!(pr_positions::CuDeviceVector{SVector{3,T},1}, pr_efields::CuDe
             wdx = ws[i] / dx
             wdy = ws[i] / dy
             wdz = ws[i] / dz
-            weight_x[i] = wdx
-            weight_y[i] = wdy
-            weight_z[i] = wdz
             xflag = abs(dx) < eps(T) ? i : xflag
             yflag = abs(dy) < eps(T) ? i : yflag
             zflag = abs(dz) < eps(T) ? i : zflag
@@ -153,33 +112,27 @@ function gpu_L2P!(pr_positions::CuDeviceVector{SVector{3,T},1}, pr_efields::CuDe
             sum_z += wdz
         end
 
-        if xflag == -1
-            weight_x ./= sum_x
-        else
-            weight_x .= 0.0
-            weight_x[xflag] = 1.0
-        end
-
-        if yflag == -1
-            weight_y ./= sum_y
-        else
-            weight_y .= 0.0
-            weight_y[yflag] = 1.0
-        end
-
-        if zflag == -1
-            weight_z ./= sum_z
-        else
-            weight_z .= 0.0
-            weight_z[zflag] = 1.0
-        end
-
         e_sum = SVector{3,T}(0.0,0.0,0.0)
         b_sum = SVector{3,T}(0.0,0.0,0.0)
         for mp_k in 1:(D+1)
+            if zflag == -1
+                wz = ws[mp_k] /(z - mp_zcoords[mp_k]) / sum_z
+            else
+                wz = (mp_k == zflag) ? 1.0 : 0.0
+            end
             for mp_j in 1:(D+1)
+                if yflag == -1
+                    wy = ws[mp_j] /(y - mp_ycoords[mp_j]) / sum_y
+                else
+                    wy = (mp_j == yflag) ? 1.0 : 0.0
+                end
                 for mp_i in 1:(D+1)
-                    weight = weight_x[mp_i] * weight_y[mp_j] * weight_z[mp_k]
+                    if xflag == -1
+                        wx = ws[mp_i] /(x - mp_xcoords[mp_i]) / sum_x
+                    else
+                        wx = (mp_i == xflag) ? 1.0 : 0.0
+                    end
+                    weight = wx * wy * wz
                     e_sum += mp_efields[mp_i,mp_j,mp_k,nodeindex] * weight
                     b_sum += mp_bfields[mp_i,mp_j,mp_k,nodeindex] * weight
                 end
