306e1edc1511065d9dea93fc875e82863f206dde
Yi-Kai Kan <tw.ykkan@gmail.com>
2022-10-16 15:31:53 +0200

    Change operations using high-level abstraction

--- src/update_particles_field.jl
+++ src/update_particles_field.jl
@@ -68,43 +68,36 @@ function update_particles_field!(particles::Particles{T}, alg::FMMGPU; lambda) w
 
     q = particles.charge
     N = particles.npar
-    p_avg = sum(particles.momenta) / particles.npar
+
+    d_pr_positions = CuArray(particles.positions)
+    d_pr_momenta = CuArray(particles.momenta)
+    d_pr_efields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),N)
+    d_pr_bfields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),N)
+
+    p_avg = reduce(+, d_pr_momenta) / N
     g_avg = sqrt(1.0 + dot(p_avg, p_avg))
     stretch = SVector(1.0,1.0,g_avg)
     
     nc = ncluster(N,N0)
     ct = ClusterTree(particles; N0=N0, stretch=stretch)
-    cl = ct.clusters
-    pr = particles
 
     d_ct_parindices = CuArray(ct.parindices)
 
-    d_cl_parlohis = CuArray(cl.parlohis)
-    d_cl_parents = CuArray(cl.parents)
-    d_cl_children = CuArray(cl.children)
-    d_cl_bboxes = CuArray(cl.bboxes)
+    d_cl_parlohis = CuArray(ct.clusters.parlohis)
+    d_cl_parents = CuArray(ct.clusters.parents)
+    d_cl_children = CuArray(ct.clusters.children)
+    d_cl_bboxes = CuArray(ct.clusters.bboxes)
 
-    d_pr_positions = CuArray(pr.positions)
-    d_pr_momenta = CuArray(pr.momenta)
-    # d_pr_efields = CuArray{SVector{3,T}}(undef,N)
-    # d_pr_bfields = CuArray{SVector{3,T}}(undef,N)
-    d_pr_efields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),N)
-    d_pr_bfields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),N)
-
-    # d_mp_gammas = CuArray{T}(undef,n+1,n+1,n+1,nc)
-    # d_mp_momenta = CuArray{SVector{3,T}}(undef,n+1,n+1,n+1,nc)
-    # d_mp_efields = CuArray{SVector{3,T}}(undef,n+1,n+1,n+1,nc)
-    # d_mp_bfields = CuArray{SVector{3,T}}(undef,n+1,n+1,n+1,nc)
     d_mp_gammas = CUDA.fill(zero(T),n+1,n+1,n+1,nc)
     d_mp_momenta = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),n+1,n+1,n+1,nc)
     d_mp_efields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),n+1,n+1,n+1,nc)
     d_mp_bfields = CUDA.fill(SVector{3,T}(0.0,0.0,0.0),n+1,n+1,n+1,nc)
 
     max_level = maxlevel(N,N0)
-
     lfindices = leafindexrange(N, N0)
     nleafnode = length(lfindices)
 
+    # upwardpass
     @cuda blocks=nleafnode threads=(n+1,n+1,n+1) gpu_P2M!(d_pr_positions, d_pr_momenta, d_ct_parindices,
                                                             d_mp_gammas, d_mp_momenta, Val(n),
                                                             d_cl_bboxes, d_cl_parlohis, lfindices)
@@ -116,11 +109,13 @@ function update_particles_field!(particles::Particles{T}, alg::FMMGPU; lambda) w
                                                             d_cl_bboxes, d_cl_children, nodeindicies)
     end
 
+    # dual-tree traversal
     itlists_gpu = InteractionListsGPU(ct.clusters; stretch=stretch, eta=eta)
-
     d_m2l_lists = CuArray(itlists_gpu.m2l_lists)
     d_m2l_lists_ptrs = CuArray(itlists_gpu.m2l_lists_ptrs)
     nm2lgroup = itlists_gpu.nm2lgroup
+
+    # interaction
     @cuda blocks=nm2lgroup threads=(n+1,n+1,n+1) gpu_M2L!(d_mp_gammas, d_mp_momenta, d_mp_efields, d_mp_bfields, Val(n),
                                                         d_cl_bboxes, d_m2l_lists, d_m2l_lists_ptrs,
                                                         p_avg)
@@ -132,6 +127,7 @@ function update_particles_field!(particles::Particles{T}, alg::FMMGPU; lambda) w
     @cuda blocks=np2pgroup threads=N0 gpu_P2P!(d_pr_positions, d_pr_momenta, d_pr_efields, d_pr_bfields, d_ct_parindices,
                                             d_cl_parlohis, Val(N0), d_p2p_lists, d_p2p_lists_ptrs)
 
+    # downwardpass
     for l in 1:max_level
         nodeindicies = nodeindexrangeat(l)
         nc_in_level = 2^l
@@ -144,10 +140,9 @@ function update_particles_field!(particles::Particles{T}, alg::FMMGPU; lambda) w
                                                 d_cl_bboxes, d_cl_parlohis, lfindices)
 
     amp = 2.8179403699772166e-15 * q / lambda
-    nt = 512
-    nb = ceil(Int, N/nt)
-    @cuda blocks=nb threads=nt gpu_update_amplitude!(d_pr_efields, d_pr_bfields, N, amp)
-    
+    d_pr_efields .*= amp
+    d_pr_bfields .*= amp
+
     copyto!(particles.efields, d_pr_efields)
     copyto!(particles.bfields, d_pr_bfields)
 end
